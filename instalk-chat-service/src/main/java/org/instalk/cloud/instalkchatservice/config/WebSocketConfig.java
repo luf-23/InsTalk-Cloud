70381287030f85a7516b3176d208bcd9629f8d1f
yzj_luf <3162794813@qq.com>
2026-04-23 00:01:32 +0800

    fix: 移除Cors改由Vite代理后端请求

--- instalk-chat-service/src/main/java/org/instalk/cloud/instalkchatservice/config/WebSocketConfig.java
+++ instalk-chat-service/src/main/java/org/instalk/cloud/instalkchatservice/config/WebSocketConfig.java
@@ -21,7 +21,6 @@ public class WebSocketConfig implements WebSocketConfigurer {
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry.addHandler(webSocketHandler, "/ws")
-                .addInterceptors(webSocketAuthInterceptor)
-                .setAllowedOrigins("*");
+                .addInterceptors(webSocketAuthInterceptor);
     }
 }
